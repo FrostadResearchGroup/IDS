f0b0e28d7483e9ed67af9fdbff015cdc6d35130b
vaastav <vaastav.anand05@gmail.com>
2017-10-01 22:15:15 -0700

    Add basic setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from distutils.core import setup, Extension
+import sys
+
+if sys.platform == "win32":
+    args = 
+    {
+        'extra_compile_args': [],
+        'library_dirs': ['C:/Program Files/IDS/uEye/Develop/Lib'],
+        'libraries': ['ueye_api', 'ueye_tools'],
+        'include_dirs': ['.', 'C:/Program Files/IDS/uEye/Deelop/include']
+    }
+else:
+    # TODO: Support this on Linux systems
+    args = {}
+
+args['sources'] = []
+
+coreExtension = Extension("core", **args)
+
+setup(
+    name = 'ids',
+    author = 'Vaastav Anand',
+    author_email = 'vaastav.anand05@gmail.com',
+    version = '1.0',
+    url = 'https://github.com/FrostadResearchGroup/IDS',
+    license = 'MIT',
+    description = 'Python Wrapper Library around IDS Camera SDK',
+    ext_modules = ['core']
+)
