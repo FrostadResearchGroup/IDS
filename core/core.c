abfcbaf3e4521956857317e416a88bdccdb42a66
vaastav <vaastav.anand05@gmail.com>
2017-10-03 15:08:58 -0700

    Add basic module init stuff

--- core/core.c
+++ core/core.c
@@ -2,4 +2,17 @@
 #include <uEye.h>
 #include "core.h"
 
-
+/*
+ * This is only needed for Python3
+ * IDS module initialization
+ * Based on https://docs.python.org/3/extending/extending.html#the-module-s-method-table-and-initialization-function
+ */
+static struct PyModuleDef idsModule = 
+{
+    PyModuleDef_HEAD_INIT,
+    "ids", /* name of module */
+    NULL,  /* module documentation */
+    -1,    /* size of perinterpreter state of the module,
+              or -1 if the module keeps state in global variables. */
+    idsMethods
+};
--- core/core.h
+++ core/core.h
@@ -3,4 +3,28 @@
 #ifndef CORE_H_INCLUDED
 #define CORE_H_INCLUDED
 
+#include<stdint.h>
+
+typedef struct
+{
+    PyObject_HEAD
+    HIDS        handle;
+    uint32_t    width;
+    uint32_t    height;
+    int         bitdepth;
+    int         color;
+    int         autofeatures;
+    int         status;
+
+} Camera;
+
+enum Status
+{
+    NOT_READY,
+    CONNECTED,
+    READY,
+};
+
+Camera* init_camera(void);
+
 #endif
