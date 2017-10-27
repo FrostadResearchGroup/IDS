c129de5c02472c84e753fe807b66d907f542d7ad
vaastav <vaastav.anand05@gmail.com>
2017-10-27 01:48:18 -0700

    Add pixel clock property to Camera

--- src/ids_camera_properties.c
+++ src/ids_camera_properties.c
@@ -254,11 +254,50 @@ PyObject * camera_get_frame_rate(Camera * self, void * closure)
     return value;
 }
 
+int camera_set_pixel_clock(Camera * self, PyObject * value, void * closure)
+{
+    int returnCode;
+    UINT nPixelClock;
+
+    if (PyLong_Check(value))
+    {
+        nPixelClock = (UINT)PyLong_AsLong(value);
+        returnCode = is_PixelClock(self->handle, IS_PIXELCLOCK_CMD_SET,(void*)&nPixelClock, sizeof(nPixelClock));
+        if (returnCode != IS_SUCCESS)
+        {
+            print_error(self);
+            return -1;
+        }
+    }
+    else
+    {
+        return -1;
+    }
+
+    return 0;
+}
+
+PyObject * camera_get_pixel_clock(Camera * self, void * closure)
+{
+    int returnCode;
+    UINT nPixelClock;
+
+    returnCode = is_PixelClock(self->handle, IS_PIXELCLOCK_CMD_GET, (void*)&nPixelClock, sizeof(nPixelClock));
+    if (returnCode != IS_SUCCESS)
+    {
+        print_error(self);
+        return NULL;
+    }
+    
+    return Py_BuildValue("i", nPixelClock);
+}
+
 PyGetSetDef camera_properties[] = {
     {"master_gain", (getter)camera_get_master_gain, (setter)camera_set_master_gain, "Master Gain", NULL},
     {"red_gain", (getter)camera_get_red_gain, (setter)camera_set_red_gain, "Red Gain", NULL},
     {"blue_gain", (getter)camera_get_blue_gain, (setter)camera_set_blue_gain, "Blue Gain", NULL},
     {"green_gain", (getter)camera_get_green_gain, (setter)camera_set_green_gain, "Green Gain", NULL},
     {"frame_rate", (getter)camera_get_frame_rate, (setter)camera_set_frame_rate, "Frame Rate", NULL},
+    {"pixel_clock", (getter)camera_get_pixel_clock, (setter)camera_set_pixel_clock, "Pixel Clock", NULL},
     {NULL} /* sentinel */
 };
