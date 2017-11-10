274e5045d4be2e76fae898cb2a0c3c55501a62a9
vaastav <vaastav.anand05@gmail.com>
2017-11-10 13:04:26 -0800

    Add get image info method

--- src/ids_camera_images.c
+++ src/ids_camera_images.c
@@ -1,5 +1,6 @@
 #include <uEye.h>
 #include "ids.h"
+#include <datetime.h>
 
 #define IMAGE_TIMEOUT 1000
 
@@ -29,7 +30,57 @@ int camera_unlock_image_queue_buffer(Camera * self, INT imgID, char * pBuffer)
 
 PyObject * camera_get_image_info(Camera * self, INT imgID, int * pRetVal)
 {
-    return NULL;
+    UEYEIMAGEINFO imageInfo;
+    int returnCode;
+    PyObject * info = PyDict_New();
+    PyObject * timestamp;
+    PyObject * digital_input;
+    PyObject * gpio1;
+    PyObject * gpio2;
+    PyObject * frame_number;
+    PyObject * camera_buffers;
+    PyObject * used_camera_buffers;
+    PyObject * height;
+    PyObject * width;
+
+    returnCode = is_GetImageInfo(self->handle, imgID, &imageInfo, sizeof(imageInfo));
+    if (returnCode != IS_SUCCESS)
+    {
+        print_error(self);
+        return NULL;
+    }
+
+    timestamp = PyDateTime_FromDateAndTime(imageInfo.TimestampSystem.wYear, imageInfo.TimestampSystem.wMonth, imageInfo.TimestampSystem.wDay, imageInfo.TimestampSystem.wHour, imageInfo.TimestampSystem.wMinute, imageInfo.TimestampSystem.wSecond, imageInfo.TimestampSystem.wMilliseconds);
+    digital_input = Py_BuildValue("I", imageInfo.dwIoStatus&4);
+    gpio1 = Py_BuildValue("I", imageInfo.dwIoStatus&2);
+    gpio2 = Py_BuildValue("I", imageInfo.dwIoStatus&1);
+    frame_number = Py_BuildValue("K", imageInfo.u64FrameNumber);
+    camera_buffers = Py_BuildValue("I", imageInfo.dwImageBuffers);
+    used_camera_buffers = Py_BuildValue("I", imageInfo.dwImageBuffersInUse);
+    height = Py_BuildValue("I", imageInfo.dwImageHeight);
+    width = Py_BuildValue("I", imageInfo.dwImageWidth);
+
+    PyDict_SetItemString(info, "timestamp", timestamp);
+    PyDict_SetItemString(info, "digital_input", digital_input);
+    PyDict_SetItemString(info, "gpio1", gpio1);
+    PyDict_SetItemString(info, "gpio2", gpio2);
+    PyDict_SetItemString(info, "frame_number", frame_number);
+    PyDict_SetItemString(info, "camera_buffers", camera_buffers);
+    PyDict_SetItemString(info, "used_camera_buffers", used_camera_buffers);
+    PyDict_SetItemString(info, "height", height);
+    PyDict_SetItemString(info, "width", width);
+
+    Py_DECREF(timestamp);
+    Py_DECREF(digital_input);
+    Py_DECREF(gpio1);
+    Py_DECREF(gpio2);
+    Py_DECREF(frame_number);
+    Py_DECREF(camera_buffers);
+    Py_DECREF(used_camera_buffers);
+    Py_DECREF(height);
+    Py_DECREF(width);
+
+    return info;
 }
 
 PyObject * camera_get_image_as_ndarray(Camera * self, char * pBuffer, int * pRetVal)
